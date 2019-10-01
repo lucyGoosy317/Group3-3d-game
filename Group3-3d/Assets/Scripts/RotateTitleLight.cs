664570482376cb8ead16fbe52e6cad482fc30c25
Lucy <luziano.reyna.jr@gmail.com>
2019-09-30 22:42:15 -0500

    Update to assest,title UI

---
+++ Group3-3d/Assets/Scripts/RotateTitleLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTitleLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x , transform.localEulerAngles.y + 1.0f*(Time.deltaTime*5), transform.localEulerAngles.z);
+    }
+}
