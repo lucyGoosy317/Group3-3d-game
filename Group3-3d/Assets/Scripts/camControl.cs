961151ff1f37007685cfb8f63f36db81bb551d70
Lucy <luziano.reyna.jr@gmail.com>
2019-10-10 23:49:10 -0500

    Player Made w/Animations

---
+++ Group3-3d/Assets/Scripts/camControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camControl : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public Vector3 camOffset;
+
+    [Range(0.01f,1.0f)]
+    public float Smoothness = 0.5f;
+
+    public bool LookAtPlayer = false;
+
+    public bool RotateAroundPlayer = true;
+    public float RotationSpeed=5.0f;
+    
+    // Update is called once per frame
+    void Start()
+    {
+        camOffset = transform.position - PlayerTransform.position;    
+    }
+
+    private void LateUpdate()
+    {
+        
+        if (RotateAroundPlayer)
+        {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationSpeed, Vector3.up);
+            camOffset = camTurnAngle * camOffset;
+
+            Vector3 newPos = PlayerTransform.position + camOffset;
+            transform.position = Vector3.Slerp(transform.position, newPos, Smoothness);
+
+            if (LookAtPlayer || RotateAroundPlayer)
+            {
+                transform.LookAt(PlayerTransform);
+            }
+        }
+
+    }
+    }
+
---
+++ Group3-3d/Assets/Scripts/charController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charController : MonoBehaviour
+
+{
+    //private CharacterController characterController;
+    //private Animator anime;
+
+    public float runSpeed = 10.0f;
+    //public float walkBackspeed = 2.0f;
+    public float rotateSpeed = 80.0f;
+    float gravity = 15;
+    float jumpHeight = 10;
+    private Vector3 moveDirection =Vector3.zero;
+    public Animator playerAnime;
+    void Start()
+    {
+        playerAnime = GetComponentInChildren<Animator>();
+
+    }
+
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(0,0,Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= runSpeed;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpHeight;
+                playerAnime.SetInteger("condition", 3);
+
+            }
+            if (Input.GetAxis("Vertical")>0)
+            {
+                playerAnime.SetInteger("condition",1);
+            }
+            if (Input.GetAxis("Vertical") < 0)
+            {
+                
+                playerAnime.SetInteger("condition",2);
+            }
+            if(Input.GetAxis("Vertical") == 0)
+            {
+                playerAnime.SetInteger("condition", 0);
+            }
+            
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+        transform.Rotate(0,Input.GetAxis("Horizontal") * rotateSpeed *Time.deltaTime,0);
+        
+
+    }
+}
