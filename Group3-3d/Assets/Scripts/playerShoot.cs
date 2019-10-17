2407b32e72084b0e0300269c5ed4d1697e957229
Lucy <luziano.reyna.jr@gmail.com>
2019-10-17 03:20:14 -0500

    update
    player can now shoot using right trigger
    w/audio, holding down will keep the firing going. The sounds will not go into culling
    A target is there to shoot at

--- Group3-3d/Assets/Scripts/playerShoot.cs
+++ Group3-3d/Assets/Scripts/playerShoot.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class playerShoot : MonoBehaviour
 {
+   // public GameObject[] childObjects;
+    public GameObject barrelLocation;
+    public AudioSource gunNoises;
+    public AudioClip gunShot;
+    bool shootingGat;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gunNoises = GetComponent<AudioSource>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        shootingGat = true;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
+    {
+        findBarrelLocation();
+        float fireTrigger = Input.GetAxis("Fire1");
+        //Debug.Log(fireTrigger);
+        if (fireTrigger>0 && shootingGat)
+        {
+            StartCoroutine(GunShootingAudio());
+            Debug.Log("Fired");
+            checkForHit();
+        }
+        
+       
+        //gunNoises.Stop();
+
+    }
+    //universal all gats will have a BarrelLocation
+    void findBarrelLocation()
     {
         
+        barrelLocation = GameObject.Find("BarrelLocation");
+        
     }
+    //testing, add if for enemy
+    void checkForHit()
+    {
+        RaycastHit objectHit;
+        Vector3 fwd = barrelLocation.transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(barrelLocation.transform.TransformDirection(Vector3.forward),fwd,Color.red);
+        if (Physics.Raycast(barrelLocation.transform.position, fwd, out objectHit, Mathf.Infinity))
+        {
+            Debug.Log("I hit :"+objectHit.collider.name);
+        }
+    }
+  
+    IEnumerator GunShootingAudio()
+    {
+        shootingGat = false;
+        gunNoises.PlayOneShot(gunShot);
+        yield return new WaitForSeconds(gunShot.length/4);
+        shootingGat = true;
+    }
+
 }
