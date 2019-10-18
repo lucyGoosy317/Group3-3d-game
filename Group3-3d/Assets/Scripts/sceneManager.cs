034fe8ed67ba329bcd88a7c8775ae12a82289807
Lucy <luziano.reyna.jr@gmail.com>
2019-10-17 19:28:46 -0500

    update
    each level has relic and exit to next level
    Music object attached to test music
    main menu will now go to testing scene on start, or exit application on exit

--- Group3-3d/Assets/Scripts/sceneManager.cs
+++ Group3-3d/Assets/Scripts/sceneManager.cs
@@ -11,29 +11,43 @@ public class sceneManager : MonoBehaviour
         Debug.Log("Entered Trigger of:"+ gameObject.name);
         if (gameObject.name=="GoToLevel1" && player.gameObject.tag=="Player" && RelicPickUp.playerHasRelic1)
         {
-            Debug.Log("going to lvl1");
+            Debug.Log("going to lvl1 from testing");
             SceneManager.LoadScene("lvl1");
+            RelicPickUp.playerHasRelic1 = false;
             Debug.Log("Player has following relics: "+ RelicPickUp.playerHasRelic1 + RelicPickUp.playerHasRelic2+ RelicPickUp.playerHasRelic3+ RelicPickUp.playerHasRelic4+ RelicPickUp.playerHasRelic5);
         }
-        if (gameObject.name == "GoToLevel2" && player.gameObject.tag == "Player" && RelicPickUp.playerHasRelic2)
+        if (gameObject.name == "GoToLevel2" && player.gameObject.tag == "Player" && RelicPickUp.playerHasRelic1)
         {
-            Debug.Log("going to lvl2");
+            Debug.Log("going to lvl2 from lvl1");
             SceneManager.LoadScene("lvl2");
         }
-        if (gameObject.name == "GoToLevel3" && player.gameObject.tag == "Player" && RelicPickUp.playerHasRelic3)
+        if (gameObject.name == "GoToLevel3" && player.gameObject.tag == "Player" && RelicPickUp.playerHasRelic2)
         {
-            Debug.Log("going to lvl3");
+            Debug.Log("going to lvl3 from lvl2");
             SceneManager.LoadScene("lvl3");
         }
-        if (gameObject.name == "GoToLevel4" && player.gameObject.tag == "Player" && RelicPickUp.playerHasRelic4)
+        if (gameObject.name == "GoToLevel4" && player.gameObject.tag == "Player" && RelicPickUp.playerHasRelic3)
         {
-            Debug.Log("going to lvl4");
+            Debug.Log("going to lvl4 from lvl3");
             SceneManager.LoadScene("lvl4");
         }
-        if (gameObject.name == "GoToLevel5" && player.gameObject.tag == "Player" && RelicPickUp.playerHasRelic5)
+        if (gameObject.name == "GoToLevel5" && player.gameObject.tag == "Player" && RelicPickUp.playerHasRelic4)
         {
-            Debug.Log("going to lvl5");
+            Debug.Log("going to lvl5 from lvl4");
             SceneManager.LoadScene("lvl5");
         }
+    }
+
+
+    public void StartGame()
+    {
+        Debug.Log("Starting game in Testing Scene");
+        SceneManager.LoadScene("TestingScene");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quiting Game");
+        Application.Quit();
     }
 }
