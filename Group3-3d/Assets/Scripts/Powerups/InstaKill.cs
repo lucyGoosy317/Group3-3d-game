c2a53d0fcdd313855eef9bbf898b53b219ef21e3
Mobyh <luisgaza95sant@gmail.com>
2019-12-05 09:33:50 -0600

    First commit

---
+++ Group3-3d/Assets/Scripts/Powerups/InstaKill.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstaKill : MonoBehaviour
+{
+    GameObject player;
+    GameObject gun;
+    PlayerShooting playerShooting;
+    GameObject graveyardPowerupManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gun = GameObject.FindGameObjectWithTag("gun");
+        player = GameObject.FindGameObjectWithTag("Player");
+        graveyardPowerupManager = GameObject.FindGameObjectWithTag("GraveyardPowerupManager");
+        playerShooting = gun.GetComponent<PlayerShooting>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerShooting.InstaKill();
+            graveyardPowerupManager.GetComponent<PowerupManager>().PowerupTaken();
+            gameObject.GetComponent<AudioSource>().Play();
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            Destroy(gameObject,3f);
+        }
+    }
+}
