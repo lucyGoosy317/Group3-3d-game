1523f77e2f126c9912cb4c2d7aa4e736a9e547ef
hunterjfox <hunterjfox6@gmail.com>
2019-12-08 17:28:20 -0600

    lvl2

---
+++ Group3-3d/Assets/Scripts/lvl2Scripts/Dance.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dance : MonoBehaviour
+{
+     private void OnTriggerEnter(Collider other)
+    {
+        
+        MeshRenderer meshRend = GetComponent<MeshRenderer>();
+        meshRend.material.color = Color.black;
+        Debug.Log(other.name);
+    }
+}
