c8ebb292441150f5e5d862370b24f9aeb09f9e8f
Lucy <luziano.reyna.jr@gmail.com>
2019-12-09 05:50:57 -0600

    update
    Scene is very coupled by scene objects

--- Group3-3d/Assets/playerKeyPadInteract.cs
+++ Group3-3d/Assets/playerKeyPadInteract.cs
@@ -1,17 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class playerKeyPadInteract : MonoBehaviour
 {
-    private GameObject KeyPadInput;
+    public GameObject KeyPadInput;
+    private playerHealth playerhealthChecker;
+    private GameObject EventSystem;
+    private ChangeEventSystem eventSystemController;
+    private bool alreadyChanged;
     // Start is called before the first frame update
     void Start()
     {
-        KeyPadInput = GameObject.FindGameObjectWithTag("keypadinput");
+        alreadyChanged = false;
+        playerhealthChecker = GetComponent<playerHealth>();
+        //KeyPadInput = GameObject.FindGameObjectWithTag("keypadinput");
+        //EventSystem = GameObject.FindGameObjectWithTag("eventSystem");
+        eventSystemController = GetComponent<ChangeEventSystem>();
+    }
+    public void Update()
+    {
+        if (alreadyChanged)
+            return;
+        if (playerhealthChecker.healthAmount<=0)
+        {
+
+            Debug.Log("Starting change");
+            eventSystemController.changeEventSystem();
+            KeyPadInput.SetActive(false);
+            alreadyChanged = true;
+        }
     }
 
-    
 
     private void OnTriggerEnter(Collider other)
     {
