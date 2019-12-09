d76b2c23ac29c20dec05225d803f6457976ed81c
Lucy <luziano.reyna.jr@gmail.com>
2019-12-09 04:44:00 -0600

    update
    fuctioning keypad controller and puzzle. added scripts to player on level not prefab. player prefab got an added variable to stop player from moving when false

--- Group3-3d/Assets/KeyPadController.cs
+++ Group3-3d/Assets/KeyPadController.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class KeyPadController : MonoBehaviour
 {
+    //player stops moving when using
+    private GameObject player;
+    public charController playerMovementController;
+
+    private GameObject KeyCodeInputUI;
+
+    private GameObject GateKeeper;
+
+    private GameObject keypadInteract;
+    private KeyPadController disableKeypad;
+    private string inputString;
+    private string correctCode;
     // Start is called before the first frame update
     void Start()
     {
+        inputString = "";
+        correctCode = "DEADBEEF";
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerMovementController = player.GetComponent<charController>();
+        GateKeeper = GameObject.FindGameObjectWithTag("gatekeeper");
+        KeyCodeInputUI = GameObject.FindGameObjectWithTag("keypadinput");
+        KeyCodeInputUI.SetActive(false);
+        keypadInteract = GameObject.FindGameObjectWithTag("keypadinteract");
         
     }
 
@@ -14,5 +34,93 @@ public class KeyPadController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ButtonA()
+    {
+        //play sound
+        inputString += "A";
+        Debug.Log("current InputString: "+inputString);
+    }
+    public void ButtonB()
+    {
+
+        inputString += "B";
+        Debug.Log("current InputString: " + inputString);
+    }
+    public void ButtonC()
+    {
+
+        inputString += "C";
+        Debug.Log("current InputString: " + inputString);
+    }
+    public void ButtonD()
+    {
+
+        inputString += "D";
+        Debug.Log("current InputString: " + inputString);
+    }
+    public void ButtonE()
+    {
+
+        inputString += "E";
+        Debug.Log("current InputString: " + inputString);
+    }
+    public void ButtonF()
+    {
+
+        inputString += "F";
+        Debug.Log("current InputString: " + inputString);
+    }
+    public void ButtonG()
+    {
+
+        inputString += "G";
+        Debug.Log("current InputString: " + inputString);
+    }
+    public void ButtonH()
+    {
+
+        inputString += "H";
+        Debug.Log("current InputString: " + inputString);
+    }
+    public void ButtonI()
+    {
+
+        inputString += "I";
+        Debug.Log("current InputString: " + inputString);
+    }
+    public void ButtonEnter()
+    {
+        if (inputString.Contains(correctCode))
+        {
+            //play sound
+            Debug.Log("Correct!");
+            playerMovementController.stopPlayerMovement = false;
+            KeyCodeInputUI.SetActive(false);
+            keypadInteract.GetComponent<Collider>().enabled = false;
+            Destroy(GateKeeper);
+
+        }
+        else
+        {
+            //play sound
+            Debug.Log("Wrong!");
+            inputString = "";
+            playerMovementController.stopPlayerMovement = false;
+            KeyCodeInputUI.SetActive(false);
+        }
+    }
+    public void ButtonClear()
+    {
+        //play sound
+        inputString = "";
+        Debug.Log("Input string is now clear: "+inputString);
+    }
+    public void ButtonExit()
+    {
+        //play sound
+        KeyCodeInputUI.SetActive(false);
+        playerMovementController.stopPlayerMovement = false;
     }
 }
---
+++ Group3-3d/Assets/playerKeyPadInteract.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerKeyPadInteract : MonoBehaviour
+{
+    private GameObject KeyPadInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        KeyPadInput = GameObject.FindGameObjectWithTag("keypadinput");
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("keypadinteract"))
+        {
+            gameObject.GetComponent<charController>().stopPlayerMovement = true;
+            KeyPadInput.SetActive(true);
+        }
+    }
+
+   
+
+
+}
