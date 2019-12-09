a6c3dc532e899250d2eb18b79a36ac2dcbf112b2
Lucy <luziano.reyna.jr@gmail.com>
2019-12-09 10:13:37 -0600

    update title

---
+++ Group3-3d/Assets/StartGameOrExit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameOrExit : MonoBehaviour
+{
+    AudioSource titleSounds;
+    public Animator fader;
+    //public AudioClip GunShot;
+    private void Start()
+    {
+        titleSounds = GetComponent<AudioSource>();
+    }
+
+    public void GoToFirstLevel()
+    {
+        StartCoroutine(waitForFading());
+        SceneManager.LoadScene("lvl1");
+    }
+
+    public void ExitGame()
+    {
+        StartCoroutine(waitForFading());
+        Application.Quit();
+    }
+
+    IEnumerator waitForFading()
+    {
+        fader.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1);
+    }
+
+}
