f9b7a7e3b21d8526a0f15bc6a9b788f9b6645518
Mobyh <luisgaza95sant@gmail.com>
2019-12-09 06:19:12 -0600

    Working level

---
+++ Group3-3d/Assets/puzzle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzle : MonoBehaviour
+{
+
+    public static puzzle instance;
+
+    public List<GameObject> pads;
+    public GameObject door;
+
+    public bool pad1 = false;
+    public bool pad2 = false;
+    public bool pad3 = false;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void padPressed(int id)
+    {
+        if(id == 1 && !pad2 && !pad3)
+        {
+            pad1 = true;
+        }
+        else if(id == 2 && pad3 && pad1)
+        {
+            pad2 = true;
+        }
+        else if((id == 3 && !pad2 && pad1))
+        {
+            pad3 = true;
+        }
+        else
+        {
+            pad1 = false;
+            pad2 = false;
+            pad3 = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pad1 && pad2 && pad3)
+        {
+            Destroy(door);
+        }
+    }
+}
